8199aca45bb254cb6d75af15bd42c23de011b692
Vinay Kumar Rai <87277004+iamvokar@users.noreply.github.com>
2024-10-26 11:52:04 +0530

    Update settings.py

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sbmcnk%^hlmg-+1ect_jm6!_$*nlrqcxs_0%mwgxq*h5&varh9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
